028afa75c161d039fc248a3270691cff5e9bc5d6
Lin <81331908+ailinwu520@users.noreply.github.com>
2021-11-23 19:30:10 +1000

    final

--- AmongUs.java
+++ AmongUs.java
@@ -11,11 +11,39 @@ public class AmongUs {
      * @return the label of the imposter or -1 if the imposter does not exist
      */
     public int findImposter(int n, int[][] suspect) {
-        //TODO Fill in this method
-        throw new UnsupportedOperationException("Not yet implemented");
+        //initialize boolean array for killer
+        boolean[] killer = new boolean[n];
+
+        // for each suspect, determine whether if there is a killer
+        for (int i = 0; i < suspect.length; i++) {
+            killer[suspect[i][0]-1] = true;
+        }
+
+        // initialize index
+        int index = -2;
+
+        // determine if the suspect is a killer
+        for (int j = 0; j < killer.length; j++) {
+            if (!killer[j]) {
+                    index = j;
+            }
+        }
+        return index + 1;
     }
 
     public static void main(String[] args) {
-        //Test your code here
+        AmongUs findKiller = new AmongUs();
+        //Input: n = 4, suspect = [[1,3],[1,4],[2,3],[2,4],[4,3]]
+        //Output: 3
+        int[][] sus1 = {{1,3},{1,4},{2,3},{2,4},{4,3}};
+        System.out.println(findKiller.findImposter(4, sus1));
+        //Input: n = 2, suspect = [[1,2]]
+        //Output: 2
+        int[][] sus2 = {{1,2}};
+        System.out.println(findKiller.findImposter(2, sus2));
+        //Input: n = 3, suspect = [[1,3],[2,3],[3,2]]
+        //Output: -1
+        int[][] sus3 = {{1,3},{2,3},{3,2},};
+        System.out.println(findKiller.findImposter(3, sus3));
     }
 }
