c0c711c6606728983ac3b44724173e1c542169da
Lin <81331908+ailinwu520@users.noreply.github.com>
2021-11-23 18:50:31 +1000

    Create AmongUs.java

---
+++ AmongUs.java
@@ -0,0 +1,21 @@
+public class AmongUs {
+    /**
+     * Find out the imposter in the spaceship,
+     * which each crew-mate suspect one other person,
+     * while the imposter suspect nobody.
+     *
+     * @param n       number of people
+     * @param suspect list of sus
+     *                i.e. n = 2, suspect = [[1,2]] -> 1 suspect 2, and 2 suspect no one
+     *                therefore 2 is the imposter
+     * @return the label of the imposter or -1 if the imposter does not exist
+     */
+    public int findImposter(int n, int[][] suspect) {
+        //TODO Fill in this method
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public static void main(String[] args) {
+        //Test your code here
+    }
+}
